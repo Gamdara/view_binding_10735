2b14aa6827a9884f2e52d3b40089379c109c1426
Gamdara <poiu12345654321@gmail.com>
2022-09-13 01:11:25 +0800

    Initial commit

---
+++ app/src/main/java/com/rama/view_binding_10735/Task.kt
@@ -0,0 +1,5 @@
+package com.rama.view_binding_10735
+
+data class Task (val chapter : String, val title:String, val details:String) {
+
+}
---
+++ app/src/main/java/com/rama/view_binding_10735/TaskList.kt
@@ -0,0 +1,11 @@
+package com.rama.view_binding_10735
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur", "turu"),
+        Task("Chapter 2", "Tidur Lagi", "turu again"),
+        Task("Chapter 3", "Bangunnnn", "awake"),
+        Task("Chapter 4", "Tidur lagi", "turu gaes"),
+        Task("Chapter 5", "Tak Gendong", "kemana mana"),
+        )
+}
